bc50e92b92c433755a2d8d7a30048a37a9939c25
Puja Moravadiya <puja.moravadiya2024@gmail.com>
2024-11-07 19:09:18 +0530

    remove layout from component

--- src/redux-toolkit/rootReducer.ts
+++ src/redux-toolkit/rootReducer.ts
@@ -4,26 +4,16 @@ import { combineReducers } from '@reduxjs/toolkit';
 // ** Redux Slices **
 import { reducer as authReducer } from './slices/authSlice';
 import { reducer as countryJsonReducer } from './slices/countryJsonSlice';
-import { reducer as filterOptionsReducer } from './slices/filtereventSlice';
-import { reducer as languageReducer } from './slices/languageSlice';
-import { reducer as isReadReducer } from './slices/notificationReadSlice';
-import { reducer as paginationReducer } from './slices/paginationSlice';
 import { reducer as rolePermissionReducer } from './slices/rolePermissionSlice';
 import { reducer as SideBarReducer } from './slices/sidebarSlice';
 import { reducer as toastReducer } from './slices/toastSlice';
-import { reducer as tokenReducer } from './slices/tokenSlice';
 import { reducer as commonReducer } from './slices/commonSlice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  language: languageReducer,
   sidebar: SideBarReducer,
-  token: tokenReducer,
-  currentPage: paginationReducer,
   rolePermission: rolePermissionReducer,
   countryJson: countryJsonReducer,
-  isRead: isReadReducer,
-  filterOptions: filterOptionsReducer,
   common: commonReducer,
   toast: toastReducer,
 });
